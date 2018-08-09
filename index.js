959e7aa57c2a98474e65f1d8dc79e95f36c967e1
Nicholas Powers <njpowers7915@gmail.com>
2018-08-09 15:39:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function addComment() {
   event.preventDefault();
-  var bodyText = document.getElementById("commentText").value;
+  var bodyText = document.getElementById("commentContent").value;
   var commenter = document.getElementById("commenterName").value;
- 
+
   var commentBody = createCommentBody(bodyText);
   var commenterLabel = createCommenterLabel(commenter);
   postNewComment(commentBody, commenterLabel);
